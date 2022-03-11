22c664d4505b1fa7d96e1351d5894775c525e372
Anthony J <antho.joyeux@gmail.com>
2022-03-11 16:55:26 +0100

    formlogin : link to formSignup

--- frontend/src/components/FormLogin.js
+++ frontend/src/components/FormLogin.js
@@ -1,48 +1,57 @@
 import * as React from "react";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
+import Box from "@mui/material/Box";
 import EmailIcon from "@mui/icons-material/Email";
 import LockIcon from "@mui/icons-material/Lock";
 import SendIcon from "@mui/icons-material/Send";
 import Button from "@mui/material/Button";
+import "./styles/FormLogin.scss";
 
 class FormLogin extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <TextField
-          id="email"
-          size="small"
-          label="Email"
-          margin="normal"
-          type="email"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <EmailIcon />
-              </InputAdornment>
-            ),
-          }}
-          variant="outlined"
-        />
-        <TextField
-          id="password"
-          size="small"
-          type="password"
-          label="Password"
-          margin="normal"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <LockIcon />
-              </InputAdornment>
-            ),
-          }}
-          variant="outlined"
-        />
-        <Button sx={{ mt: "20px" }} variant="contained" endIcon={<SendIcon />}>
-          Login
-        </Button>
+        <Box className="formLogin">
+          <TextField
+            id="email"
+            size="small"
+            label="Email"
+            margin="normal"
+            type="email"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <EmailIcon />
+                </InputAdornment>
+              ),
+            }}
+            variant="outlined"
+          />
+          <TextField
+            id="password"
+            size="small"
+            type="password"
+            label="Password"
+            margin="normal"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <LockIcon />
+                </InputAdornment>
+              ),
+            }}
+            variant="outlined"
+          />
+          <Button sx={{ mt: "20px" }} variant="contained" endIcon={<SendIcon />}>
+            Login
+          </Button>
+          <p>
+            <a class="link" href="">
+              Pas encore de compte ?
+            </a>
+          </p>
+        </Box>
       </React.Fragment>
     );
   }
