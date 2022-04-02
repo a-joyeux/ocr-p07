d2ff71b6ff02745959522135da457e95c8ebdb9d
Anthony J <antho.joyeux@gmail.com>
2022-04-02 13:48:00 +0200

    some comments layout tweaks

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -2,30 +2,32 @@ import * as React from 'react';
 import './styles/Comment.scss';
 
 function Comment(comments) {
-  return (
-    <div className='comment-list'>
-      <span className='comment-title'>Commentaires</span>
-      {comments.map((comment) => {
-        return (
-          <div className='comment'>
-            <div className='comment-header'>
-              <span className='author'>{comment.User.firstName + ' ' + comment.User.lastName}</span>
-              <span className='date'>
-                {new Date(comment.createdAt).toLocaleDateString(navigator.language, {
-                  hour: 'numeric',
-                  minute: 'numeric',
-                  day: 'numeric',
-                  month: 'long',
-                  year: 'numeric',
-                })}
-              </span>
+  if (comments.length > 0) {
+    return (
+      <div className='comment-list'>
+        <span className='comment-title'>Commentaires</span>
+        {comments.map((comment) => {
+          return (
+            <div className='comment'>
+              <div className='comment-header'>
+                <span className='author'>{comment.User.firstName + ' ' + comment.User.lastName}</span>
+                <span className='date'>
+                  {new Date(comment.createdAt).toLocaleDateString(navigator.language, {
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    day: 'numeric',
+                    month: 'long',
+                    year: 'numeric',
+                  })}
+                </span>
+              </div>
+              <div className='comment-content'>{comment.content}</div>
             </div>
-            <div className='comment-content'>{comment.content}</div>
-          </div>
-        );
-      })}
-    </div>
-  );
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default Comment;
