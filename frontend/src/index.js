de31b8183fa536baf99910d2b9cb0fddc6a26a9d
Anthony J <antho.joyeux@gmail.com>
2022-04-09 16:45:18 +0200

    remove old post page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './views/HomePage';
-import NewPostPage from './views/NewPostPage';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,7 +11,6 @@ ReactDOM.render(
       <Routes>
         <Route path='/' element={<App />} />
         <Route path='/home' element={<HomePage />} />
-        <Route path='/post' element={<NewPostPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
